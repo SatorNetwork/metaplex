5520efeae4fa80b051fa9e70e3f10d372a23b1a3
olegliallin <olegliallin@gmail.com>
2022-05-10 12:42:03 +0300

    Updates

---
+++ _next/static/kl5MWtQsownknjkUaExHD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-3e49d3b5028fc8acffcc.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
